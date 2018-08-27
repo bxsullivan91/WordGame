883ea093334c604b60956dafe3b2ecdb176b219a
bxsullivan91 <bxsullivan91@gmail.com>
2018-08-27 15:25:44 -0500

    Word Game

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,11 +1,25 @@
 package controllers;
 
 import models.Words;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameController extends Controller
 {
+    private FormFactory formFactory;
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+    }
     public Result getStart()
     {
         return ok(views.html.start.render());
@@ -16,7 +30,80 @@ public class GameController extends Controller
         Words words = new Words();
         words.setPlace1("Parking Lot");
         words.setAdjective1("fuzzy");
+        words.setNoun1("frog");
+        words.setNoun2("zombie pit");
+        words.setPastTenseVerb1("loved");
+        words.setPlace2("the void");
 
         return ok(views.html.story.render(words));
     }
+
+    public Result getSecondStory()
+    {
+        Words words = new Words();
+        words.setPlace1("House of Mirrors");
+        words.setAdjective1("insane");
+        words.setNoun1("erector set");
+        words.setNoun2("Marvel Cinematic Universe");
+        words.setPastTenseVerb1("shot");
+        words.setPlace2("the Matrix");
+
+        return ok(views.html.story.render(words));
+
+    }
+
+    public Result getThirdStory()
+    {
+        Words words = new Words();
+        words.setPlace1("National Archives");
+        words.setAdjective1("boring");
+        words.setNoun1("mosquito");
+        words.setNoun2("van");
+        words.setPastTenseVerb1("tripped");
+        words.setPlace2("dentist");
+
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getRandomStory()
+    {
+        Words words = new Words();
+
+        words.setPlace1(words.getRandomPlace());
+        words.setAdjective1(words.getRandomAdjective());
+        words.setNoun1(words.getRandomNoun());
+        words.setNoun2(words.getRandomNoun());
+        words.setPastTenseVerb1(words.getRandomVerbPT());
+        words.setPlace2(words.getRandomPlace());
+
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getMyOwnStory()
+    {
+        return ok(views.html.selectwords.render());
+    }
+
+    public Result postMyOwnStory()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+        Words words = new Words();
+
+        String place1 = form.get("place1");
+        String adjective1 = form.get("adjective1");
+        String noun1 = form.get("noun1");
+        String noun2 = form.get("noun2");
+        String verb1 = form.get("verb1");
+        String place2 = form.get("place2");
+
+        words.setPlace1(place1);
+        words.setAdjective1(adjective1);
+        words.setNoun1(noun1);
+        words.setNoun2(noun2);
+        words.setPastTenseVerb1(verb1);
+        words.setPlace2(place2);
+
+        return ok(views.html.story.render(words));
+    }
+
 }
--- app/models/Words.java
+++ app/models/Words.java
@@ -1,9 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Words
 {
     private String place1;
     private String adjective1;
+    private String noun1;
+    private String noun2;
+    private String pastTenseVerb1;
+    private String place2;
+
 
     public String getPlace1()
     {
@@ -24,4 +32,128 @@ public class Words
     {
         this.adjective1 = adjective1;
     }
+
+    public String getNoun1()
+    {
+        return noun1;
+    }
+
+    public void setNoun1(String noun1)
+    {
+        this.noun1 = noun1;
+    }
+
+    public String getNoun2()
+    {
+        return noun2;
+    }
+
+    public void setNoun2(String noun2)
+    {
+        this.noun2 = noun2;
+    }
+
+    public String getPastTenseVerb1()
+    {
+        return pastTenseVerb1;
+    }
+
+    public void setPastTenseVerb1(String pastTenseVerb1)
+    {
+        this.pastTenseVerb1 = pastTenseVerb1;
+    }
+
+    public String getPlace2()
+    {
+        return place2;
+    }
+
+    public void setPlace2(String place2)
+    {
+        this.place2 = place2;
+    }
+
+    public String getRandomNoun()
+    {
+        Random random = new Random();
+
+        ArrayList<String> nouns = new ArrayList<>();
+        nouns.add("percentage");
+        nouns.add("delivery van");
+        nouns.add("chapter");
+        nouns.add("priority queue");
+        nouns.add("technology");
+        nouns.add("insurance card");
+        nouns.add("Bite Squad Driver");
+
+        int randomNounIndex = random.nextInt(nouns.size());
+
+        String randomNoun = nouns.get(randomNounIndex);
+
+        return randomNoun;
+
+    }
+
+    public String getRandomAdjective()
+    {
+        Random random = new Random();
+
+        ArrayList<String> adjectives = new ArrayList<>();
+        adjectives.add("serious");
+        adjectives.add("pleasant");
+        adjectives.add("significant");
+        adjectives.add("willing");
+        adjectives.add("emotional");
+        adjectives.add("conscious");
+        adjectives.add("unlikely");
+
+        int randomIndex = random.nextInt(adjectives.size());
+
+        String randomAdjective = adjectives.get(randomIndex);
+
+        return randomAdjective;
+    }
+
+    public String getRandomPlace()
+    {
+        Random random = new Random();
+
+        ArrayList<String> places = new ArrayList<>();
+        places.add("Fords of Isen");
+        places.add("Hobbiton");
+        places.add("Iceland");
+        places.add("Pawnee, Indiana");
+        places.add("that place in Office Space");
+        places.add("actual space");
+        places.add("Neverland");
+
+        int randomIndex = random.nextInt(places.size());
+
+        String randomPlace = places.get(randomIndex);
+
+        return randomPlace;
+    }
+
+    public String getRandomVerbPT()
+    {
+        Random random = new Random();
+
+        ArrayList<String> verbsPT = new ArrayList<>();
+        verbsPT.add("rode");
+        verbsPT.add("punished");
+        verbsPT.add("sunk");
+        verbsPT.add("prompted");
+        verbsPT.add("transferred");
+        verbsPT.add("praised");
+        verbsPT.add("scratched");
+
+        int randomIndex = random.nextInt(verbsPT.size());
+
+        String randomVerbPT = verbsPT.get(randomIndex);
+
+        return randomVerbPT;
+    }
+
+
 }
+
